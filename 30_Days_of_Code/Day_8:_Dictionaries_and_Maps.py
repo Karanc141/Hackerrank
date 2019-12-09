d8ffe7065e0942fbbc3d6a5d5ab34e88c3cdce09
Karanc141 <34730345+Karanc141@users.noreply.github.com>
2019-12-09 23:16:07 +0530

    To add python 3 code and multiple quries support

--- 30_Days_of_Code/Day_8:_Dictionaries_and_Maps.py
+++ 30_Days_of_Code/Day_8:_Dictionaries_and_Maps.py
@@ -39,3 +39,21 @@ for j in range(N):
         print "Not found"
     else:
         print str(b) + "=" + str(phone_book[b])
+        
+        
+        
+# for python3
+#Following code can take N Dictionary Inputs but multiple quries
+N = int(input())
+phone_book = {}
+
+for i in range(N):
+    a = input().split()
+    phone_book[a[0]] = a[1]
+  
+while input!='':
+    b = str(input())
+    if b not in phone_book:
+        print ("Not found")
+    else:
+        print (str(b) + "=" + str(phone_book[b]))
